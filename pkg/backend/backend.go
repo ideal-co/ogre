19ef92b6302763101a4a0f566bd1e839a6fd90a5
Lowell Mower <lowell.mower@gmail.com>
2020-05-12 12:51:00 -0400

    reduce backend interface to minimal necessary for the moment

--- pkg/backend/backend.go
+++ pkg/backend/backend.go
@@ -1,13 +1,27 @@
 package backend
 
-type PlatformType string
-
-const (
-    Pometheus PlatformType = "prometheus"
-    StdOut PlatformType = "stdout"
-    Grafana PlatformType = "grafana"
+import (
+	"github.com/lowellmower/ogre/pkg/config"
+	msg "github.com/lowellmower/ogre/pkg/message"
+	"github.com/lowellmower/ogre/pkg/types"
 )
 
 type Platform interface {
-    String(string) string
+	Type() types.PlatformType
+	BackendClient
+}
+
+type BackendClient interface {
+	Send(msg.Message) error
+}
+
+func NewBackendClient(pType types.PlatformType, addr string) (Platform, error) {
+	switch pType {
+	case types.StatsdBackend:
+		prefix := config.Daemon.GetString("backends.statsd.server.prefix")
+		return NewStatsdClient(addr, prefix)
+	default:
+		// TODO (lmower): return some default backend type here
+		return nil, nil
+	}
 }
