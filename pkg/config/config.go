8d2c239a9a2e13ec62536ccd8f7d6d023519589f
Lowell Mower <lowell.mower@gmail.com>
2020-05-20 11:58:02 -0400

    make config initialization more stable

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"encoding/json"
 	"github.com/lowellmower/ogre/pkg/install"
 	"github.com/moogar0880/venom"
 	"io/ioutil"
@@ -8,38 +9,141 @@ import (
 	"path/filepath"
 )
 
+/*
+NOTE: the backend config stanza
+  "backends": [
+    {
+      "type": "statsd",
+      "server": "127.0.0.1:8125",
+      "prefix": "ogre"
+    }
+  ]
+*/
+
+var DefaultConfigConst = `
+{
+  "dockerd_socket": "/run/docker.sock",
+  "containerd_socket": "/run/containerd/containerd.sock",
+  "ogred_socket": "/var/run/ogred.sock",
+  "ogred_pid": "/etc/ogre/ogred.pid",
+  "log": {
+    "level": "trace",
+    "file": "/var/log/ogred.log",
+    "silent": false,
+    "report_caller": false
+  },
+  "services": [
+    {
+      "type": "docker",
+      "log": {
+        "level": "info",
+        "file": "/var/log/service.log",
+        "silent": false,
+        "report_caller": false
+      }
+    }
+  ]
+}
+`
+
+// DaemonConfig is the structural representation of a ogred.conf.json file.
+type DaemonConfig struct {
+	DockerdSocket    string          `json:"dockerd_socket,omitempty"`
+	ContainerdSocket string          `json:"containerd_socket,omitempty"`
+	OgredSocket      string          `json:"ogred_socket"`
+	OgredPID         string          `json:"ogred_pid"`
+	Log              LogConfig       `json:"log"`
+	Backends         []BackendConfig `json:"backends,omitempty"`
+	Services         []ServiceConfig `json:"services,omitempty"`
+}
+
+// LogConfig is the structural representation of the config for the application's
+// global Logger instances.
+type LogConfig struct {
+	Level        string `json:"level"`
+	File         string `json:"file"`
+	Silent       bool   `json:"silent"`
+	ReportCaller bool   `json:"report_caller"`
+}
+
+// BackendConfig is the structural representation of all possible fields which
+// could be used to configure a backend. Required fields do not have the
+// omitempty decorator and must be present.
+type BackendConfig struct {
+	Type   string `json:"type"`
+	Server string `json:"server"`
+	Prefix string `json:"prefix,omitempty"`
+	Job    string `json:"job,omitempty"`
+	Metric string `json:"metric,omitempty"`
+}
+
+// ServiceConfig is the structural representation of a service in the config
+// file. At the time of writing this, the only exposed service is docker and
+// that service is also enabled by default.
+type ServiceConfig struct {
+	Type string    `json:"type"`
+	Log  LogConfig `json:"log"`
+}
+
+// our application wide config values
 var Daemon = &venom.Venom{}
 var Service = &venom.Venom{}
 
+// init for the config package will attempt to load config from the default file
+// path or, if that does not exist, load it from the string literal of defaults
+// above and then write that to said path.
 func init() {
-	writeDefaultConfig()
-	Daemon = readConfig(install.HostConfigDir + install.OgredConfig)
-	Service = readConfig(install.HostConfigDir + install.ServiceConfig)
+	// check if config file exists
+	ogredConf := install.HostConfigDir + install.OgredConfig
+	if confFile, _ := os.Stat(ogredConf); confFile != nil {
+		// if the file exists on host, load that file
+		LoadConfig()
+		return
+	}
+
+	// if no config existed on host, load default and write config
+	LoadDefaults()
 }
 
-func writeDefaultConfig() {
-	confDir := install.AppRoot + install.AppConfigDir
-	files, err := ioutil.ReadDir(confDir)
-	if err != nil {
-		panic("could not read ogre configuration: " + err.Error())
+// LoadConfig makes the assumption there is a file in place at the default file
+// path for config, /etc/ogre/ogre.d/ogred.conf.json
+func LoadConfig() {
+	Daemon = readConfig(install.HostConfigDir + install.OgredConfig)
+	// TODO (lmower): determine use case for separate service config
+	//                duplicating daemon config for now.
+	Service = readConfig(install.HostConfigDir + install.OgredConfig)
+}
+
+// LoadDefaults will parse the literal string above into the DaemonConfig
+// struct, write that to a file on the host and then set the
+func LoadDefaults() {
+	writeFromMemory()
+	Daemon = readConfig(install.HostConfigDir + install.OgredConfig)
+	// TODO (lmower): determine use case for separate service config
+	//                duplicating daemon config for now.
+	Service = readConfig(install.HostConfigDir + install.OgredConfig)
+}
+
+func writeFromMemory() {
+	var conf = &DaemonConfig{}
+	if err := json.Unmarshal([]byte(DefaultConfigConst), conf); err != nil {
+		panic(err)
 	}
-	for _, f := range files {
-		data, err := ioutil.ReadFile(confDir + f.Name())
-		if err != nil {
-			panic("could not read ogre configuration: " + err.Error())
+	hostFile := filepath.Join(install.HostConfigDir, install.OgredConfig)
+	if _, err := os.Stat(hostFile); err != nil {
+		if err = os.MkdirAll(install.HostConfigDir, os.FileMode(os.O_RDWR)); err != nil {
+			panic("config dir did not exist and could not be created: " + err.Error())
 		}
-		hostFile := filepath.Join(install.HostConfigDir, f.Name())
-		if _, err := os.Stat(hostFile); err != nil {
-			if err = os.MkdirAll(install.HostConfigDir, os.FileMode(os.O_RDWR)); err != nil {
-				panic("config dir did not exist and could not be created: " + err.Error())
-			}
-			if _, err = os.Create(hostFile); err != nil {
-				panic("config file did not exist and could not be created: " + err.Error())
-			}
+		if _, err = os.Create(hostFile); err != nil {
+			panic("config file did not exist and could not be created: " + err.Error())
 		}
-		if err = ioutil.WriteFile(hostFile, data, os.FileMode(os.O_RDWR)); err != nil {
-			panic("could not read ogre configuration: " + err.Error())
-		}
+	}
+	data, err := json.MarshalIndent(conf, "", "    ")
+	if err != nil {
+		panic("could not unmarshal default config: " + err.Error())
+	}
+	if err = ioutil.WriteFile(hostFile, data, os.FileMode(os.O_RDWR)); err != nil {
+		panic("could not write ogre configuration: " + err.Error())
 	}
 }
 
